3d4d9730df91a5015402cbaf1c133cc5d948dc9b
bahar <101214031+baharraksu@users.noreply.github.com>
2023-05-18 13:30:03 +0300

    Problemsiz çalışıyor.

--- app/src/main/java/com/bahar/eogrenci/LoginActivity.java
+++ app/src/main/java/com/bahar/eogrenci/LoginActivity.java
@@ -71,5 +71,6 @@ public class LoginActivity extends AppCompatActivity {
         });
 
 
+
     }
 }
---
+++ app/src/main/java/com/bahar/eogrenci/TartismaActivity.java
@@ -0,0 +1,14 @@
+package com.bahar.eogrenci;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TartismaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tartisma);
+    }
+}
