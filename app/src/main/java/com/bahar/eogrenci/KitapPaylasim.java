a9ca1595ce612cc8843882b223b1544e6ea62f50
bahar <101214031+baharraksu@users.noreply.github.com>
2023-05-21 23:55:08 +0300

    Kitap paylaşım sayfayı yapıldı.Database eklenmeye hazır.

--- app/src/main/java/com/bahar/eogrenci/KitapPaylasim.java
+++ app/src/main/java/com/bahar/eogrenci/KitapPaylasim.java
@@ -1,14 +1,34 @@
 package com.bahar.eogrenci;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class KitapPaylasim extends AppCompatActivity {
+    RecyclerView recyclerView;
+    FloatingActionButton add_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kitap_paylasim);
+
+        recyclerView=findViewById(R.id.recyclerView);
+        add_button=findViewById(R.id.add_button);
+
+        add_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(KitapPaylasim.this,AddActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
