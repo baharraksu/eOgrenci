2dec42e7881c93a9f0be9d42d2a15eb953cfc168
bahar <101214031+baharraksu@users.noreply.github.com>
2023-05-23 00:28:06 +0300

    Veri tabanına eklenen kitaplar görüntülenebiliyor.

--- app/src/main/java/com/bahar/eogrenci/KitapPaylasim.java
+++ app/src/main/java/com/bahar/eogrenci/KitapPaylasim.java
@@ -2,33 +2,65 @@ package com.bahar.eogrenci;
 
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 public class KitapPaylasim extends AppCompatActivity {
     RecyclerView recyclerView;
     FloatingActionButton add_button;
+    MyDatabaseHelper myDB;
+    ArrayList<String> book_id, book_title, book_author, book_pages;
+    CustomAdapter customAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kitap_paylasim);
 
-        recyclerView=findViewById(R.id.recyclerView);
-        add_button=findViewById(R.id.add_button);
+        recyclerView = findViewById(R.id.recyclerView);
+        add_button = findViewById(R.id.add_button);
 
         add_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(KitapPaylasim.this,AddActivity.class);
+                Intent intent = new Intent(KitapPaylasim.this, AddActivity.class);
                 startActivity(intent);
             }
         });
 
+        myDB = new MyDatabaseHelper(KitapPaylasim.this);
+        book_id = new ArrayList<>();
+        book_title = new ArrayList<>();
+        book_author = new ArrayList<>();
+        book_pages = new ArrayList<>();
+        storeDataInArrays();
+        customAdapter = new CustomAdapter(KitapPaylasim.this, this, book_id, book_title, book_author,
+                book_pages);
+        recyclerView.setAdapter(customAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(KitapPaylasim.this));
+    }
+
+    void storeDataInArrays() {
+        Cursor cursor = myDB.readAllData();
+        if (cursor.getCount() == 0) {
+            Toast.makeText(this, "No data.", Toast.LENGTH_SHORT).show();
+        } else {
+            while (cursor.moveToNext()) {
+                book_id.add(cursor.getString(0));
+                book_title.add(cursor.getString(1));
+                book_author.add(cursor.getString(2));
+                book_pages.add(cursor.getString(3));
+            }
+        }
     }
 }
